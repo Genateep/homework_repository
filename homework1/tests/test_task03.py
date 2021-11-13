f10a8313dd402f71aee1287f17f28349367f0e24
Gennady Guniashev <genateepspb@gmail.com>
2021-11-13 11:59:01 +0300

    task03 and test_task03 added

---
+++ homework1/tests/test_task03.py
@@ -0,0 +1,14 @@
+import pytest
+
+from homework1.task03 import find_maximum_and_minimum
+
+
+@pytest.mark.parametrize(
+    "file_name, (min, max)",
+    [
+        ("some_file.txt", (0, 7)),
+        # ("another_file.txt", (0, 9))
+    ]
+)
+def test_positive_case(file_name, min_num, max_num):
+    assert find_maximum_and_minimum(file_name) == (min_num, max_num)
