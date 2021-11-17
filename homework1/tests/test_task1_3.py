0f799095bd35e049d84f32b9932de4ef7a79714c
Gennady Guniashev <genateepspb@gmail.com>
2021-11-17 21:05:01 +0300

    Update test_task1_3

--- homework1/tests/test_task1_3.py
+++ homework1/tests/test_task1_3.py
@@ -2,10 +2,12 @@ import pytest
 
 from homework1.task03 import find_maximum_and_minimum
 
+file = 'homework1/tests/source/some_file.txt'
+
 
 def test_find_maximum_and_minimum():
-    assert find_maximum_and_minimum("homework1/tests/source/some_file.txt") == (7, -1)
+    assert find_maximum_and_minimum(file) == (7, -1)
 
 
 def test_find_maximum_and_minimum_negative_case():
-    assert not find_maximum_and_minimum("homework1/tests/source/some_file.txt") == (1, 2)
+    assert not find_maximum_and_minimum(file) == (1, 2)
