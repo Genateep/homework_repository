34aee35d220d675ce02a7551e192a788ccc195d8
Gennady Guniashev <genateepspb@gmail.com>
2021-11-26 22:59:11 +0300

    task01 updated

--- homework3/task01.py
+++ homework3/task01.py
@@ -2,22 +2,29 @@ from typing import Callable
 
 
 def timed_cache(times: int):
-    def cache(func: Callable) -> Callable:
-        """Accepts another function as an argument. Then it
-        should return such a function, so the every call to initial one
-        should be cached for n times unless it receives any other arguments"""
-        cached = dict()
+    """Accepts another function as an argument. Then it
+    should return such a function, so the every call to initial one
+    should be cached for n times unless it receives any other arguments"""
+    cached, cached_times = {}, {}
 
+    def cache(function: Callable):
         def decorate(*args, **kwargs):
+
             key = (tuple(args), hash(tuple(sorted(kwargs.items()))))
-            if key not in cached:
-                cached[key] = func(*args, **kwargs)
-                cached['times'] = times
-            else:
-                cached['times'] -= 1
-            if cached['times'] <= 0:
-                return func(*args, **kwargs)
-            return cached[key]
+
+            if key in cached_times:
+                res = cached[key]
+
+                if cached_times[key] <= 1:
+                    del cached_times[key]
+                    del cached[key]
+                else:
+                    cached_times[key] -= 1
+                return res
+            res = function(*args, **kwargs)
+            cached_times[key] = times
+            cached[key] = res
+            return res
 
         return decorate
 
--- homework3/tests/test_task01.py
+++ homework3/tests/test_task01.py
@@ -3,4 +3,4 @@ from homework3.task01 import some_func
 
 def test_some_func():
     a, b, c, d, e = (some_func(15, 6) for _ in range(5))
-    assert a is b and b is c and c is not d and d is not e
+    assert a is b and b is c and c is d and d is not e
