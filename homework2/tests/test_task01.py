8f0161d3c6a5f37c8c59a35750a7a3ed03c63d14
Gennady Guniashev <genateepspb@gmail.com>
2021-11-26 21:43:19 +0300

    test_task01 source dir updated

--- homework2/tests/test_task01.py
+++ homework2/tests/test_task01.py
@@ -2,7 +2,7 @@ from homework2.task01 import (count_non_ascii_chars, count_punctuation_chars,
                               get_longest_diverse_words,
                               get_most_common_non_ascii_char, get_rarest_char)
 
-file_path = "source/data.txt"
+file_path = "homework2/tests/source/data.txt"
 
 
 def test_get_longest_diverse_words():
