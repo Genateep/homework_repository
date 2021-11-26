75fef8fba30bb70da00f2139fe259c57b55c4fa6
Gennady Guniashev <genateepspb@gmail.com>
2021-11-26 21:20:47 +0300

    task01 updated

--- homework2/tests/test_task01.py
+++ homework2/tests/test_task01.py
@@ -1,15 +1,9 @@
-from homework2.task01 import (
-    count_non_ascii_chars,
-    count_punctuation_chars,
-    get_longest_diverse_words,
-    get_most_common_non_ascii_char,
-    get_rarest_char,
-)
+from homework2.task01 import *
 
 file_path = "source/data.txt"
 
 
-def test_get_longest_diverse_words():
+def test_get_longest_diverse_words(get_longest_diverse_words):
     assert get_longest_diverse_words(
         file_path,
         encoding='unicode-escape'
@@ -27,28 +21,28 @@ def test_get_longest_diverse_words():
     ]
 
 
-def test_get_rarest_char():
+def test_get_rarest_char(get_rarest_char):
     assert get_rarest_char(
         file_path,
         encoding='unicode-escape'
     ) == '›'
 
 
-def test_count_punctuation_chars():
+def test_count_punctuation_chars(count_punctuation_chars):
     assert count_punctuation_chars(
         file_path,
         encoding='unicode-escape'
     ) == 5475
 
 
-def test_count_non_ascii_chars():
+def test_count_non_ascii_chars(count_non_ascii_chars):
     assert count_non_ascii_chars(
         file_path,
         encoding='unicode-escape'
     ) == 2971
 
 
-def test_get_most_common_non_ascii_char():
+def test_get_most_common_non_ascii_char(get_most_common_non_ascii_char):
     assert get_most_common_non_ascii_char(
         file_path,
         encoding='unicode-escape'
