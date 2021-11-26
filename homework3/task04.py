804724f932c126babac8fb6e25785873285fe03d
Gennady Guniashev <genateepspb@gmail.com>
2021-11-27 00:37:22 +0300

    task04 updated

--- homework3/task04.py
+++ homework3/task04.py
@@ -2,4 +2,4 @@ def is_armstrong(number: int) -> bool:
     """Returns True if given number is Armstrong number.
     Only positive integers allowed
     """
-    return number == sum([int(_)**len(str(number)) for _ in str(number)])
+    return number == sum([int(i)**len(str(number)) for i in str(number)])
