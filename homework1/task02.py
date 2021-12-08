38bd9977824aec6e8fbef7bcfbb993c993e84f90
Gennady Guniashev <genateepspb@gmail.com>
2021-12-08 17:42:16 +0300

    task02 upd

--- homework1/task02.py
+++ homework1/task02.py
@@ -2,20 +2,14 @@ from typing import Sequence
 
 
 def check_fib(data: Sequence[int]) -> bool:
-
     """Returns if the given sequence is a Fibonacci sequence"""
-    if len(data) >= 3:
-        for i in data:
-            if data.index(i) == 0 and i == 0:
-                continue
-            elif data.index(i) == 1 and i == 1:
-                continue
-            elif (
-                data.index(i) > 1
-                    and i == data[data.index(i) - 1] + data[data.index(i) - 2]
-            ):
-                continue
-            else:
-                return False
 
-        return True
+    for index, value in enumerate(data):
+
+        if index in (0, 1):
+            if index == value:
+                continue
+            return False
+        if value != data[index - 1] + data[index - 2]:
+            return False
+    return True
