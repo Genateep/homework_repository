0ec5777bb948b43395d3be86a52d2bcd54858c22
Gennady Guniashev <genateepspb@gmail.com>
2021-12-12 15:33:18 +0300

    task02 upd

--- homework7/task02.py
+++ homework7/task02.py
@@ -17,9 +17,13 @@ Examples:
     Output: False
     Explanation: s becomes "c" while t becomes "b".
 """
+import re
 
 
 def backspace_compare(first: str, second: str) -> bool:
+    """Returns if strings are equal when both are typed into
+    empty text editors
+    """
     f, s = [], []
     for char in first:
         if char != '#':
@@ -32,3 +36,18 @@ def backspace_compare(first: str, second: str) -> bool:
         elif s:
             s.pop()
     return f == s
+
+
+def backspace_regexp_compare(first: str, second: str) -> bool:
+    """Returns if strings are equal when both are typed into
+    empty text editors
+    """
+
+    def _apply_backspace(s):
+        while True:
+            t = re.sub('.#', '', s, count=1)
+            if len(s) == len(t):
+                return re.sub('#+', '', t)
+            s = t
+
+    return _apply_backspace(first) == _apply_backspace(second)
--- homework7/tests/test_task02.py
+++ homework7/tests/test_task02.py
@@ -1,4 +1,4 @@
-from homework7.task02 import backspace_compare
+from homework7.task02 import backspace_compare, backspace_regexp_compare
 
 
 def test_backspace_compare_positive():
@@ -11,3 +11,15 @@ def test_backspace_compare_positive():
 def test_backspace_compare_negative():
     assert not backspace_compare("a#c", "b")
     assert not backspace_compare("a##c", "#ac#")
+
+
+def test_backspace_regexp_compare_positive():
+    assert backspace_regexp_compare("ab#c", "ad#c")
+    assert backspace_regexp_compare("a##c", "#a#c")
+    assert backspace_regexp_compare("#", "")
+    assert backspace_regexp_compare("", "")
+
+
+def test_backspace_regexp_compare_negative():
+    assert not backspace_regexp_compare("a#c", "b")
+    assert not backspace_regexp_compare("a##c", "#ac#")
