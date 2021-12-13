dc5eef6d32210e4f52286eb98878bf94e412e68f
Gennady Guniashev <genateepspb@gmail.com>
2021-12-13 11:16:49 +0300

    task01 added

--- homework8/task01.py
+++ homework8/task01.py
@@ -4,17 +4,38 @@ each line is represented as key and value separated by = symbol, example:
 name=kek last_name=top song_name=shadilay power=9001
 
 Values can be strings or integer numbers.
-If a value can be treated both as a number and a string, it is treated as number.
+If a value can be treated both as a number and a string,
+it is treated as number.
 
 Write a wrapper class for this key value storage that works like this:
 
 storage = KeyValueStorage('path_to_file.txt')
 that has its keys and values accessible as collection items and as attributes.
-Example: storage['name'] # will be string 'kek' storage.song_name # will be 'shadilay'
+Example: storage['name'] # will be string 'kek'
+storage.song_name # will be 'shadilay'
 storage.power # will be integer 9001
 
 In case of attribute clash existing built-in attributes take precedence.
 In case when value cannot be assigned to an attribute
 (for example when there's a line 1=something) ValueError should be raised.
-File size is expected to be small, you are permitted to read it entirely into memory.
+File size is expected to be small,
+you are permitted to read it entirely into memory.
 """
+
+
+class KeyValueStorage:
+    def __init__(self, path: str):
+        existing_builtins = set(self.__dict__.keys())
+
+        with open(path) as file:
+            for item in file.read().split():
+                key, value = item.split("=", maxsplit=1)
+                if key not in existing_builtins and key.isidentifier():
+                    if value.isdigit():
+                        value = int(value)
+                    self.__dict__[key] = value
+                else:
+                    raise ValueError(f"{item} unacceptable")
+
+    def __getitem__(self, key: str):
+        return self.__dict__[key]
