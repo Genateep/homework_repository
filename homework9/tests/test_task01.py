68359c72f2601b25aed18842892b62995f0e6402
Gennady Guniashev <genateepspb@gmail.com>
2021-12-19 15:24:32 +0300

    test_task01 upd

--- homework9/tests/test_task01.py
+++ homework9/tests/test_task01.py
@@ -1,16 +1,20 @@
 from homework9.task01 import merge_sorted_files as m_f
 
+file1 = 'homework9/tests/file1.txt'
+file2 = 'homework9/tests/file2.txt'
+file3 = 'homework9/tests/file3.txt'
+
 
 def test_merge_sorted_files():
-    assert list(m_f(['file1.txt', 'file2.txt'])) == [1, 2, 3, 4, 5, 6]
+    assert list(m_f([file1, file2])) == [1, 2, 3, 4, 5, 6]
 
 
 def test_merge_sorted_files_negative_numbers():
-    assert list(m_f(['file1.txt', 'file3.txt'])) == [-5, -3, -1, 1, 3, 5]
+    assert list(m_f([file1, file3])) == [-5, -3, -1, 1, 3, 5]
 
 
 def test_merge_sorted_files_three_files():
-    assert list(m_f(['file1.txt', 'file2.txt', 'file3.txt'])) == [
+    assert list(m_f([file1, file2, file3])) == [
         -5,
         -3,
         -1,
