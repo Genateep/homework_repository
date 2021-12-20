bed4f2bd3593bf8f67d04990759afd5584620a3b
Gennady Guniashev <genateepspb@gmail.com>
2021-12-20 18:11:26 +0300

    task03 upd

--- homework9/task03.py
+++ homework9/task03.py
@@ -1,6 +1,8 @@
 """
-Write a function that takes directory path, a file extension and an optional tokenizer.
-It will count lines in all files with that extension if there are no tokenizer.
+Write a function that takes directory path,
+a file extension and an optional tokenizer.
+It will count lines in all files with that extension
+if there are no tokenizer.
 If a the tokenizer is not none, it will count tokens.
 
 For dir with two files from hw1.py:
