23a008e6471d04e108a8a78a34318a489384d04c
Gennady Guniashev <genateepspb@gmail.com>
2022-01-11 15:52:20 +0300

    hw12 init

---
+++ homework12/task01/polls/migrations/one_db_record.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    def db_recording(apps, schema_editor):
+        Homework = apps.get_model("polls", "Homework")
+        HomeworkResult = apps.get_model("polls", "HomeworkResult")
+        Student = apps.get_model("polls", "Student")
+        Teacher = apps.get_model("polls", "Teacher")
+
+        student = Student(first_name="Lev", last_name="Sokolov")
+        homework = Homework(
+            text="Learn OOP", deadline=timedelta(days=7), created=datetime.now()
+        )
+        result = HomeworkResult(
+            homework=homework,
+            author=student,
+            solution="Done",
+            created=datetime.now(),
+        )
+        teacher = Teacher(first_name="Daniil", last_name="Shadrin", homework_done=result)
+
+        student.save()
+        homework.save()
+        result.save()
+        teacher.save()
+
+    dependencies = [
+        ("polls", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(db_recording),
+    ]
---
+++ homework12/task01/polls/tests.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from polls.models import Homework, HomeworkResult, Student, Teacher
+
+
+class UserTest(TestCase):
+    fixtures = ["fixture.json"]
+
+    def test_student(self):
+        student = Student.objects.get(id=1)
+        self.assertEqual(student.first_name, "Lev")
+        self.assertEqual(student.last_name, "Sokolov")
+
+    def test_teacher(self):
+        teacher = Teacher.objects.get(id=1)
+        self.assertEqual(teacher.first_name, "Daniil")
+        self.assertEqual(teacher.last_name, "Shadrin")
+        self.assertEqual(teacher.homework_done.id, 1)
+
+
+class HomeworkTest(TestCase):
+    fixtures = ["fixture.json"]
+
+    def test_homework(self):
+        homework = Homework.objects.get(id=1)
+        self.assertEqual(homework.text, "Learn OOP")
+        self.assertEqual(homework.created.date().isoformat(), "2021-01-10")
+        self.assertEqual(homework.deadline.days, 7)
+
+    def test_homework_result(self):
+        result = HomeworkResult.objects.get(id=1)
+        self.assertEqual(result.homework.text, "Learn OOP")
+        self.assertEqual(result.author.id, 1)
+        self.assertEqual(result.solution, "Done")
+        self.assertEqual(result.created.date().isoformat(), "2021-01-10")
